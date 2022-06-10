87a09b57fec5dea518212ace6ca61055249d6112
rahylei <rahylei@Rahylei.localdomain>
2022-06-10 13:59:57 +0000

    branc of dev

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,9 +11,10 @@
     </head>
     <body class="">
         <h1>This the welcome of the application... :)</h1>
+        <h2>By the way... is this the brach of dev</h2>
 
         <label for="checked-toggle" class="inline-flex relative items-center cursor-pointer">
-            <input type="checkbox" value="" id="checked-toggle" class="sr-only peer" checked="" onclick="getStat()">
+            <input type="checkbox" value="" id="checked-toggle" class="sr-only peer" checked="false" onclick="getStat()">
             <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
             <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Checked toggle</span>
         </label>
